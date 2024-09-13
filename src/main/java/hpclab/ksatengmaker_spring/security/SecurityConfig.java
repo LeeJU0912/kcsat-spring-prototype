3868c1ff39d6556ed775930dc790346ce9a364a3
LeeJU <thirdspirit@naver.com>
2024-09-13 16:10:11 +0900

    게시글 문제 보기 추가

--- src/main/java/hpclab/ksatengmaker_spring/security/SecurityConfig.java
+++ src/main/java/hpclab/ksatengmaker_spring/security/SecurityConfig.java
@@ -2,12 +2,20 @@ package hpclab.ksatengmaker_spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +28,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_USER");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
@@ -27,12 +40,12 @@ public class SecurityConfig {
                 .headers((headerConfig) -> headerConfig
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(authorize -> authorize
-                                // 페이지 권한 설정
-                                .requestMatchers("/member/**").hasRole("ADMIN")
-                                .requestMatchers("/board/post/**").hasRole("USER")
-                                .requestMatchers("/question/**").hasRole("USER")
-                                .requestMatchers("/myBook/**").hasRole("USER")
-                                .anyRequest().permitAll()
+                        // 페이지 권한 설정
+                        .requestMatchers("/member/**").hasRole("ADMIN")
+                        .requestMatchers("/board/post/**").hasRole("USER")
+                        .requestMatchers("/question/**").hasRole("USER")
+                        .requestMatchers("/myBook/**").hasRole("USER")
+                        .anyRequest().permitAll()
                 )
                 .formLogin((authorize) -> authorize
                         .loginPage("/login")
@@ -49,6 +62,28 @@ public class SecurityConfig {
                 .exceptionHandling((exception) -> exception
                         .accessDeniedPage("/denied")
                 );
+
+//        http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .inMemoryAuthentication()
+//                .withUser("HPCLab1@csatmaker.site")
+//                .password(passwordEncoder().encode("HPCLab1"))
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("HPCLab2@csatmaker.site")
+//                .password(passwordEncoder().encode("HPCLab2"))
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("HPCLab3@csatmaker.site")
+//                .password(passwordEncoder().encode("HPCLab3"))
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("HPCLab4@csatmaker.site")
+//                .password(passwordEncoder().encode("HPCLab4"))
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("HPCLab5@csatmaker.site")
+//                .password(passwordEncoder().encode("HPCLab5"))
+//                .roles("ADMIN");
 
         return http.build();
     }
